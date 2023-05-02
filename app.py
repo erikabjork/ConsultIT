c1d5a9e4f8bd64ea07de2c3238ea2643c022977d
Josef <joseflundbergs@gmail.com>
2023-05-02 12:42:20 +0200

    Version1.8

--- app.py
+++ app.py
@@ -1,15 +1,5 @@
 import streamlit as st
 
-st.title("Title")
-st.write("Text")
-
-st.write("Hej")
-st.write("Test2")
-st.write("hallå")
-st.write('häst')
-number = st.slider("Pick a number", 0, 100)
-
-
 import requests
 import json
 
