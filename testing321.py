79692f45b033af0befed7cd7b63c7a1ce5103d8e
LudwigJL <ludwig.j.lundborg@gmail.com>
2023-05-04 17:35:42 +0200

    Fix(repetitiv kod), kompetens lista med funk loop

--- testing321.py
+++ testing321.py
@@ -8,82 +8,52 @@ url_for_search = f"{url}/search"
 url2 = 'https://dev-historical-api.jobtechdev.se'
 url_for_search2 = f"{url2}/search"
 
-
-def _get_ads(params):
+# Merge de två get_ads funktionerna så de är i samma pot. Vid Return är (Response1 = Current APIn) och (Response2 = Historical APIn). 
+def get_ads(params1, params2):
     headers = {'accept': 'application/json'}
-    response = requests.get(url_for_search, headers=headers, params=params)
-    response.raise_for_status()  # check for http errors
-    return json.loads(response.content.decode('utf8'))
+    response1 = requests.get(url_for_search, headers=headers, params=params1)
+    response2 = requests.get(url_for_search2, headers=headers, params=params2)
+    response1.raise_for_status()  # check for http errors
+    response2.raise_for_status()  # check for http errors
+    return json.loads(response1.content.decode('utf8')), json.loads(response2.content.decode('utf8'))
 
-def _get_ads2(params2):
-    headers = {'accept': 'application/json'}
-    response = requests.get(url_for_search2, headers=headers, params=params2)
-    response.raise_for_status()  # check for http errors
-    return json.loads(response.content.decode('utf8'))
+# Sökningen sker i samma function men är separerade efter API sökning. (search params = Response1 vilker ät current) och (search_params2 = response2 vilker är historical).
+# Return är två hit nummer utefter current och historical. 
 
 def example_search_return_number_of_hits(query):
-    # limit: 0 means no ads, just a value of how many ads were found.
+
     search_params = {'q': query}
-    json_response = _get_ads(search_params)
-    number_of_hits = json_response['total']['value']
-    #print(f"\nNumber of hits = {number_of_hits}")
-    #st.write("CURRENT")
-    #st.write(number_of_hits)
-    return number_of_hits
-
-def example_search_return_number_of_hits2(query):
-    # limit: 0 means no ads, just a value of how many ads were found.
     search_params2 = {'q': query}
-    json_response = _get_ads2(search_params2)
-    number_of_hits2 = json_response['total']['value']
-    #print(f"\nNumber of hits = {number_of_hits}")
-    #st.write("HISTORICAL")
-    #st.write(number_of_hits)
-    return number_of_hits2
+    json_response1, json_response2 = get_ads(search_params, search_params2)
+    number_of_hits_current = json_response1['total']['value']
+    number_of_hits_historical = json_response2['total']['value']
+    return number_of_hits_current, number_of_hits_historical
 
-#def example_search_loop_through_hits(querystr):
-    # limit = 100 is the max number of hits that can be returned.
-    # If there are more (which you find with ['total']['value'] in the json response)
-    # you have to use offset and multiple requests to get all ads.
-    search_params = {'q': querystr}
-    json_response = _get_ads(search_params)
-    hits = json_response['hits']
-    ord = "säljare"
-    lagra = []
-    for hit in hits:
-            headline = hit['headline']
-            if ord in headline:
-                lagra.append(headline)
-                st.write(lagra)
-    print(lagra)
-
-
-        #st.write(f"{hit['publication_date']}, {hit['headline']}")
-        #print(f"{hit['headline']}, {hit['employer']['name']}")
-
-#def example_search_loop_through_hits2(query):
-    # limit = 100 is the max number of hits that can be returned.
-    # If there are more (which you find with ['total']['value'] in the json response)
-    # you have to use offset and multiple requests to get all ads.
-    search_params = {'q': query, 'limit': 100}
-    json_response = _get_ads2(search_params)
-    hits = json_response['hits']
-    #for hit in hits:
-
-
+# Skapar lista med kompetenser. Vi ska självklart lägga till fler men detta är bara för testing. 
+competencies = ['python', 'java', 'C++', 'MongoDB']
 
 if __name__ == '__main__':
-    query = st.text_input("Sök på en kompetens: ")
-    querystr = str(query)
-    #example_search_loop_through_hits(query)
-    #example_search_loop_through_hits2(query)
-    #example_search_return_number_of_hits(query)
-    #example_search_return_number_of_hits2(query)
-    number_of_hits = example_search_return_number_of_hits(query)
-    number_of_hits2 = example_search_return_number_of_hits2(query)
-    st.title("Du har sökt på: " + query)
-    st.write("CURRENT")
-    st.write(number_of_hits)
-    st.write("_____________________")
-    st.write("HISTORICAL")
-    st.write(number_of_hits2)
+    results = {}
+    # For loop som går igenom alla kompetenser, var för sig. Lägger in just den kompetensen som query, gör en sökning både historical och current.
+    # Precis som input sökning bara att den går igenom en lista full med kompetenser som input. 
+    for query in competencies:
+        number_of_hits_current, number_of_hits_historical = example_search_return_number_of_hits(query)
+        results[query] = (number_of_hits_current, number_of_hits_historical)
+    
+    # Detta går att gör snyggare men hittade ingen annat sätt. Känns som att vi kommer behöva automatisera det. 
+    python_current, python_historical = results['python']
+    java_current, java_historical = results['java']
+    cplusplus_current, cplusplus_historical = results['C++']
+    mongodb_current, mongodb_historical = results['MongoDB']
+
+
+#Testar om variablerna fungerar. 
+    print('python current: ', python_current)
+    print('python historical: ', python_historical)
+    print('java current: ', java_current)
+    print('java historical', java_historical)
+    print('C++ current: ', cplusplus_current)
+    print('C++ historical', cplusplus_historical)
+
+#Just nu funkar bara print i terminalen. Gör så för att det tar tid att öppna app.py hela tiden. 
+
