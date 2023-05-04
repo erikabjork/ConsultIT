44fb3ce7952e816add18b884e1dc226ebf1e4c00
Josef <joseflundbergs@gmail.com>
2023-05-04 13:25:05 +0200

    testversion2

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
-
 import requests
 import json
 
 url = 'https://historical.api.jobtechdev.se'
 url_for_search = f"{url}/search"
+
+url2 = 'https://jobsearch.api.jobtechdev.se'
+url2_for_search = f"{url}/search"
 
 
 def _get_ads(params):
@@ -13,10 +15,16 @@ def _get_ads(params):
     response.raise_for_status()  # check for http errors
     return json.loads(response.content.decode('utf8'))
 
+def _get_ads2(params2):
+    headers = {'accept': 'application/json'}
+    response = requests.get(url2_for_search, headers=headers, params=params2)
+    response.raise_for_status()  # check for http errors
+    return json.loads(response.content.decode('utf8'))
+
 
 def example_search_return_number_of_hits(query):
     # limit: 0 means no ads, just a value of how many ads were found.
-    search_params = {'q': query, 'limit': 100}
+    search_params = {'q': query, 'limit': 10}
     json_response = _get_ads(search_params)
     number_of_hits = json_response['total']['value']
     print(f"\nNumber of hits = {number_of_hits}")
@@ -28,11 +36,10 @@ def example_search_loop_through_hits(querystr):
     # limit = 100 is the max number of hits that can be returned.
     # If there are more (which you find with ['total']['value'] in the json response)
     # you have to use offset and multiple requests to get all ads.
-    search_params = {'q': querystr, 'limit': 100}
+    search_params = {'q': querystr, 'limit': 10}
     json_response = _get_ads(search_params)
     hits = json_response['hits']
-    #print(hits)
-    word = "2017"
+    word = "2021"
     for hit in hits:
             headline = hit['headline']
             pub = hit['publication_date']
@@ -43,9 +50,30 @@ def example_search_loop_through_hits(querystr):
                 st.write(f"{hit['publication_date']}, {hit['headline']}")
         #st.write(f"{hit['headline']}")
 
+def example_search_loop_through_hits2(querystr2):
+    # limit = 100 is the max number of hits that can be returned.
+    # If there are more (which you find with ['total']['value'] in the json response)
+    # you have to use offset and multiple requests to get all ads.
+    search_params = {'q': querystr2, 'limit': 10}
+    json_response = _get_ads2(search_params)
+    hits = json_response['hits']
+    print(hits)
+    word = "2021"
+    for hit in hits:
+            headline = hit['headline']
+            pub = hit['publication_date']
+            if word in pub:
+            #print(f"{hit['headline']}")
+                print(headline)
+        #st.write(f"{['headline']}, {['year']['name']}")
+                st.write(f"{hit['publication_date']}, {hit['headline']}")
+        #st.write(f"{hit['headline']}")
 
 if __name__ == '__main__':
-    query = st.text_input("Skriv något här: ")
+    query = st.text_input("Titel: ")
+    query2 = st.text_input("År: ")
     querystr = str(query)
+    querystr2 = str(query)
     example_search_loop_through_hits(querystr)
+    example_search_loop_through_hits2(querystr)
     example_search_return_number_of_hits(querystr)
