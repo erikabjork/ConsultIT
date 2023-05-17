262327da8225abb5d18e61f22ebf5ffeb803fe9f
Josef <joseflundbergs@gmail.com>
2023-05-17 11:09:52 +0200

    version2.4

--- app.py
+++ app.py
@@ -1,84 +1,65 @@
 import streamlit as st
 import requests
-import json
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
-url = 'https://jobsearch.api.jobtechdev.se'
-url_for_search = f"{url}/search"
+# JobTech Historical API base URL
+BASE_URL = "https://dev-historical-api.jobtechdev.se"
 
-url2 = 'https://dev-historical-api.jobtechdev.se'
-url_for_search2 = f"{url2}/search"
+def fetch_job_ads(query, year):
+    endpoint = f"/search?limit=100&offset=0&q={query}&published-after={year}-01-01T00:00:00&published-before={year}-12-31T00:00:00"
+    url = BASE_URL + endpoint
+    response = requests.get(url)
+    return response.json() if response.status_code == 200 else None
 
+def display_job_ads(job_ads):
+    if job_ads is None:
+        st.write("Error fetching job ads. Please try again.")
+    elif not job_ads["hits"]:
+        st.write("No job ads found for the specified query and year.")
+    else:
+        total_hits = job_ads['total']['value']
+        #st.write(f"Total job ads found: {total_hits}")
+        return total_hits
 
+def plot_linear_regression(x, y, query):
+    model = LinearRegression().fit(x, y)
+    x_future = np.array(range(2016, 2026)).reshape((-1, 1))
+    y_predicted = model.predict(x_future)
 
+    plt.figure()
+    plt.scatter(x, y, color='black', label='Hits')
+    plt.plot(x_future, y_predicted, color='red', label='Trendline')
+    plt.xlabel('Year')
+    plt.ylabel('Number of Ads')
+    plt.title('Linear Regression of ' + query)
+    plt.legend()
 
-def _get_ads(params):
-    headers = {'accept': 'application/json'}
-    response = requests.get(url_for_search, headers=headers, params=params)
-    response.raise_for_status()  # check for http errors
-    return json.loads(response.content.decode('utf8'))
+    predicted_value_2024 = int(round(y_predicted[8]))  # Predicted value for 2024 (index 8)
+    st.write("Predicted value for 2024:", predicted_value_2024)
 
-def _get_ads2(params2):
-    headers = {'accept': 'application/json'}
-    response = requests.get(url_for_search2, headers=headers, params=params2)
-    response.raise_for_status()  # check for http errors
-    return json.loads(response.content.decode('utf8'))
+    return plt
 
-def example_search_return_number_of_hits(querystr):
-    # limit: 0 means no ads, just a value of how many ads were found.
-    json_response = _get_ads(querystr)
-    number_of_hits = json_response['total']['value']
-    #print(f"\nNumber of hits = {number_of_hits}")
-    #st.write("CURRENT")
-    #st.write(number_of_hits)
-    return number_of_hits
+# Streamlit app
+def main():
+    st.title("Job Ads Explorer")
+    query = st.text_input("Enter a query (e.g., python)", max_chars=100)
+    if st.button("Search"):
+        hits_by_year = {}
+        for year in range(2016, 2023):
+            job_ads = fetch_job_ads(query, year)
+            hits = display_job_ads(job_ads)
+            hits_by_year[year] = hits
 
-def example_search_return_number_of_hits2(querystr):
-    # limit: 0 means no ads, just a value of how many ads were found.
-    json_response = _get_ads2(querystr)
-    number_of_hits2 = json_response['total']['value']
-    return number_of_hits2
+        #st.write("Hits by Year:")
+        for year, hits in hits_by_year.items():
+            st.write(f"{year}: {hits}")
 
-#def example_search_loop_through_hits(querystr):
-    # limit = 100 is the max number of hits that can be returned.
-    # If there are more (which you find with ['total']['value'] in the json response)
-    # you have to use offset and multiple requests to get all ads.
-    search_params = {'q': querystr}
-    json_response = _get_ads(search_params)
-    hits = json_response['hits']
-    ord = st.text_input("Välj ett år: ")
-    for hit in hits:
-            pub = hit['publication_date']
-            if ord in pub:
-                st.write(pub)
+        x = np.array(list(hits_by_year.keys())).reshape((-1, 1))
+        y = np.array(list(hits_by_year.values()))
+        fig = plot_linear_regression(x, y, query)
+        st.pyplot(fig)
 
-        #st.write(f"{hit['publication_date']}, {hit['headline']}")
-        #print(f"{hit['headline']}, {hit['employer']['name']}")
-
-#def example_search_loop_through_hits2(query):
-    # limit = 100 is the max number of hits that can be returned.
-    # If there are more (which you find with ['total']['value'] in the json response)
-    # you have to use offset and multiple requests to get all ads.
-    search_params = {'q': query, 'limit': 100}
-    json_response = _get_ads2(search_params)
-    hits = json_response['hits']
-    #for hit in hits:
-
-
-
-if __name__ == '__main__':
-    query = st.text_input("Sök på en kompetens: ")
-    #year = st.text_input("Sök på ett år: ")
-    #yearstr = str(year)
-    querystr = str(query)
-    #example_search_loop_through_hits(query)
-    #example_search_loop_through_hits2(query)
-    #example_search_return_number_of_hits(query)
-    #example_search_return_number_of_hits2(query)
-    number_of_hits = example_search_return_number_of_hits(query)
-    number_of_hits2 = example_search_return_number_of_hits2(query)
-    st.title("Du har sökt på: " + query)
-    st.write("CURRENT")
-    st.write(number_of_hits)
-    st.write("_____________________")
-    st.write("HISTORICAL")
-    st.write(number_of_hits2)
+if __name__ == "__main__":
+    main()
