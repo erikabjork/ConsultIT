c69c7fc3f90d8d6986dca2993d9aa9da844f302d
FilippaG <130976624+FilippaG@users.noreply.github.com>
2023-05-17 11:04:24 +0200

    version3.4Clean

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import streamlit as st
-
 import requests
 import json
+import subprocess
+import numpy as np
+import altair as alt
+import pandas as pd
+from streamlit_option_menu import option_menu
+
 
 url = 'https://historical.api.jobtechdev.se'
 url_for_search = f"{url}/search"
-
-
 
 
 def _get_ads(params):
@@ -24,8 +27,6 @@ def example_search_return_number_of_hits(query):
     print(f"\nNumber of hits = {number_of_hits}")
     st.write(number_of_hits)
     
-
-
 
 def example_search_loop_through_hits(querystr):
     # limit = 100 is the max number of hits that can be returned.
@@ -61,11 +62,7 @@ def example_search_loop_through_hits(querystr):
    # example_search_return_number_of_hits(querystr)
 
 
-import streamlit as st
-from streamlit_option_menu import option_menu
-
-
-def main():
+def menu():
     # Add the logo image
        # Add the logo
     logo_path = "https://www.akademiskahogtider.se/digitalAssets/818/c_818170-l_3-k_lo_gu_cen2r294c.png"
@@ -90,6 +87,7 @@ def main():
             st.markdown("<hr>", unsafe_allow_html=True)
             st.write('Applikationen visar vad som kortsiktigt efterfrågas på arbetsmarknaden och utifrån det är tanken att kunna anpassa valbara kurser på det systemvetenskapliga programmet.')
             st.markdown("<hr>", unsafe_allow_html=True)
+
         elif choose == 'Om ConsultIT':
             st.markdown("<hr>", unsafe_allow_html=True)
             st.write('Vi är ett konsultföretag som hjälper blablablabla')
@@ -104,25 +102,13 @@ def main():
             st.markdown("<hr>", unsafe_allow_html=True)
 
 if __name__ == '__main__':
-    main()
+    menu()
 
 
 
-
-
-
-
-
-    
-# Create a beta container for the search box
 search_container = st.container()
-
-# Add a search box to the container
 search_query = search_container.text_input('Search', value='', key='search' )
 
-
-
-# Add some CSS styling to position the search box in the top right corner
 search_container.markdown("""
     <style>
         .st-bq {
@@ -161,23 +147,6 @@ search_container_styles_hover = '''
 st.write("<style>div.row-widget.stsearch_container > search_container:first-child:active { %s }</style>" % search_container_styles_active, unsafe_allow_html=True)
 st.write("<style>div.row-widget.stsearch_container > search_container:first-child:hover { %s }</style>" % search_container_styles_hover, unsafe_allow_html=True)
 
-
-import streamlit as st
-
-#from streamlit_image_coordinates import streamlit_image_coordinates
-
-#value = streamlit_image_coordinates("")
-
-
-#st.write(value)
-
-
-  
-import streamlit as st
-
-
-
-# Increase the size of the button using custom CSS styles
 button_styles = f'''
     height: 100px;
     width: 200px;
@@ -186,8 +155,6 @@ button_styles = f'''
     border: 2px solid black;
     transition: border-color 0.3s ease-in-out;
        
-
-
     '''
 button_clicked_styles = f'''
     height: 100px;
@@ -209,21 +176,17 @@ st.write("<style>div.row-widget.stButton > button:first-child:hover { %s }</styl
 st.write("<style>div.row-widget.stButton > button:first-child:active{ %s }</style>" % button_styles_active, unsafe_allow_html=True)
 st.write("<style>div.row-widget.stButton > button:first-child { %s }</style>" % button_styles, unsafe_allow_html=True)
 
-import subprocess
-
-import pandas as pd
-
 def main():
-    current_page = st.session_state.get("current_page", "page1")
+    current_page = st.session_state.get("current_page", "page1",)
 
     if current_page == "page1":
         page1()
+        
     elif current_page == "page2":
         page2()
-        
+
 def page1():    
-   # query = st.text_input("Skriv något här: ")
-    clicked = True
+    #clicked = True
     #querystr = str(query)
     #example_search_loop_through_hits(querystr)
     #example_search_return_number_of_hits(querystr)
@@ -235,15 +198,11 @@ def page1():
             col1.empty()
             st.stop()
             
-            
-
     with col2:
         if st.button("Machine learning"):
             page2()
             col2.empty()
             st.stop()
-
-        
 
     with col3:
         if st.button('Deep learning'):
@@ -251,13 +210,6 @@ def page1():
             col3.empty()
             st.stop()
         
-  
-  
-
-
-
-    
-      
     st.markdown(
     """
     <style>
@@ -288,14 +240,8 @@ def page1():
 #competencies = ['Artificial intelligence', 'Machine learning', 'Deep learning', 'Natural language processing', 'Computer vision', 'Robotics', 'Internet of Things', 'Blockchain technology', 'Augmented reality', 'Virtual reality', 'Quantum computing', 'Big data analytics', 'Cloud computing', 'Edge computing', 'Cybersecurity technologies', 'Predictive analytics', 'Autonomous vehicles', 'Genetic engineering', '3D printing/additive manufacturing', 'Advanced materials science', 'Renewable energy technologies', 'Smart grids', 'Biometrics', 'Wearable technologies', 'Nanotechnology', 'Cognitive computing', 'Swarm intelligence', 'Synthetic biology', 'Human-computer interaction', 'Data visualization, ’Analytics tools']
 #for compenetcy in competencies:
     #st.write(compenetcy)
-
-
-
     col7, col8, col9 = st.columns(3)
   
-
-
-
     with col7:
         
         st.markdown("<h2 class='small-header'>Emergent Technologies</h2>", unsafe_allow_html=True)
@@ -327,15 +273,6 @@ def page1():
         st.text('Data visualization')
         st.text('Analytics tools')
 
-        
-
-
-               
-        
-        
-
-
-
     with col8:
         st.markdown("<h2 class='small-header'>Aktiva annonser</h2>", unsafe_allow_html=True)
         st.text("800")
@@ -365,63 +302,29 @@ def page1():
         st.text("345")
         st.text("254")
 
-
-
     with col9:
-
         st.markdown("<h2 class='small-header'>Läs mer</h2>", unsafe_allow_html=True)
-        link = "<a href='#' onclick='window.open(\"http://localhost:8501/?app=page2\")' class='custom-button' style='color:white; padding:0.1rem 0.2rem;'>Om Java</a>"
+        link = "<a href='#' onclick='window.open(\"http://localhost:8501/?app=page2\")' class='custom-button' style='color:white; padding:0.1rem 0.2rem;'>Om NLP</a>"
         st.markdown(link, unsafe_allow_html=True)
         st.session_state.current_page = "page2"
         st.session_state.current_file = "page2.py"
             #st.session_state.current_page = "page2"
             #st.session_state.current_file = "page2.py"
 
-        link = "<a href='#' onclick='window.open(\"http://localhost:8501/?app=page2\")' class='custom-button' style='color:white; padding:0.1rem 0.2rem;'>Om Cloud</a>"
+        link = "<a href='#' onclick='window.open(\"http://localhost:8501/?app=page2\")' class='custom-button' style='color:white; padding:0.1rem 0.2rem;'>Om Computer vision</a>"
         st.markdown(link, unsafe_allow_html=True)
         st.session_state.current_page = "page2"
         st.session_state.current_file = "page2.py"
 
-        link = "<a href='#' onclick='window.open(\"http://localhost:8501/?app=page2\")' class='custom-button' style='color:white; padding:0.1rem 0.2rem;'>Om Python</a>"
+        link = "<a href='#' onclick='window.open(\"http://localhost:8501/?app=page2\")' class='custom-button' style='color:white; padding:0.1rem 0.2rem;'>Om Robotics</a>"
         st.markdown(link, unsafe_allow_html=True)
         st.session_state.current_page = "page2"
         st.session_state.current_file = "page2.py"
 
-
-import numpy as np
-import altair as alt
-
-should_execute_main = True
-
-
-   
 def page2():
     subprocess.Popen(["streamlit", "run", "page2.py"], shell=True)
     st.title("LineChart")
     if st.button("Back"):
         st.session_state.current_page = "page1"
-        st.session_state.current_file = "app.py"
-
-
-
-
-   
-
-def page5():
-    
-    st.title("LineChart")
-    if st.button("Back"):
-        st.session_state.current_page = "page1"
-        st.session_state.current_file = "app.py"
-        import pandas as pd
-#import streamlit as st
-#from matplotlib import pyplot as plt
-
-def page3():
-    st.write("Hej!")
-
-
-
-        
-
+        st.session_state.current_file = "app.py"  
 main()
